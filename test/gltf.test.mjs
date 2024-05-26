9929b1b8b8a96d02ec0605513db1b2492c9a4019
MatsErdkamp <m.w.g.erdkamp@student.tue.nl>
2024-05-26 16:38:48 +0200

    added a (failing) test.. investigating

--- test/gltf.test.mjs
+++ test/gltf.test.mjs
@@ -1,52 +1,67 @@
-import { expect } from "chai"
+import { expect } from "chai";
 
 describe("glTF", () => {
-
   it("should render separate correctly using pixi *.*.*", async () => {
     let render = (renderer, resources) => {
-      let model = PIXI3D.Model.from(resources["assets/teapot/teapot-separate.gltf"].gltf)
-      model.y = -0.8
-      model.meshes.forEach(mesh => {
-        mesh.material.unlit = true
-      })
-      renderer.render(model)
-    }
+      let model = PIXI3D.Model.from(
+        resources["assets/teapot/teapot-separate.gltf"].gltf
+      );
+      model.y = -0.8;
+      model.meshes.forEach((mesh) => {
+        mesh.material.unlit = true;
+      });
+      renderer.render(model);
+    };
     await expect(render).to.match("snapshots/wdhdo.png", {
-      resources: [
-        "assets/teapot/teapot-separate.gltf"
-      ]
-    })
-  })
+      resources: ["assets/teapot/teapot-separate.gltf"],
+    });
+  });
 
   it("should render binary correctly using pixi *.*.*", async () => {
     let render = async (renderer, resources) => {
-      let model = PIXI3D.Model.from(resources["assets/teapot/teapot-binary.glb"].gltf)
-      model.y = -0.8
-      model.meshes.forEach(mesh => {
-        mesh.material.unlit = true
-      })
-      renderer.render(model)
-    }
+      let model = PIXI3D.Model.from(
+        resources["assets/teapot/teapot-binary.glb"].gltf
+      );
+      model.y = -0.8;
+      model.meshes.forEach((mesh) => {
+        mesh.material.unlit = true;
+      });
+      renderer.render(model);
+    };
     await expect(render).to.match("snapshots/wdhdo.png", {
-      resources: [
-        "assets/teapot/teapot-binary.glb"
-      ]
-    })
-  })
+      resources: ["assets/teapot/teapot-binary.glb"],
+    });
+  });
 
   it("should render embedded correctly using pixi *.*.*", async () => {
     let render = (renderer, resources) => {
-      let model = PIXI3D.Model.from(resources["assets/teapot/teapot-embedded.gltf"].gltf)
-      model.y = -0.8
-      model.meshes.forEach(mesh => {
-        mesh.material.unlit = true
-      })
-      renderer.render(model)
-    }
+      let model = PIXI3D.Model.from(
+        resources["assets/teapot/teapot-embedded.gltf"].gltf
+      );
+      model.y = -0.8;
+      model.meshes.forEach((mesh) => {
+        mesh.material.unlit = true;
+      });
+      renderer.render(model);
+    };
     await expect(render).to.match("snapshots/wdhdo.png", {
-      resources: [
-        "assets/teapot/teapot-embedded.gltf"
-      ]
-    })
-  })
-})
+      resources: ["assets/teapot/teapot-embedded.gltf"],
+    });
+  });
+
+  it("should render meshopt correctly using pixi *.*.*", async () => {
+    let render = (renderer, resources) => {
+      let model = PIXI3D.Model.from(
+        resources["assets/teapot/teapot-binary-meshopt.glb"].gltf
+      );
+      model.y = -0.8;
+      model.meshes.forEach((mesh) => {
+        mesh.material.unlit = true;
+      });
+      renderer.render(model);
+    };
+    await expect(render).to.match("snapshots/wdhdo.png", {
+      resources: ["assets/teapot/teapot-binary-meshopt.glb"],
+    });
+  });
+});
